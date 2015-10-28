749221e4fe04b56553f533ed99ec51bc9b13187d
devstopfix <james@devstopfix.net>
2015-10-28 09:38:16 +0000

    wonderland

--- wonderland-number/project.clj
+++ wonderland-number/project.clj
@@ -1,6 +1,6 @@
-(defproject wonderland-number "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject wonderland-number "0.1.1"
+  :description "There is a six digit wonderland number that is quite strange"
+  :url "https://github.com/gigasquid/wonderland-clojure-katas/tree/master/wonderland-number"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,23 @@
 (ns wonderland-number.finder)
 
+(defn numbers-have-same-digits? [n1 n2]
+  "Return true iff n1 and n2 have all the same digits"
+  (= (set (str n1)) (set (str n2))))
+
+(defn multiple-has-same-digits? [n m]
+  "Return true if m•n has same digits as n"
+  (numbers-have-same-digits? n (* n m)))
+
+(defn wonder? [n]
+  "Return true iff n is a wonder number"
+  (and
+    (= 6 (count (str n)))
+    (every? #(multiple-has-same-digits? n %)
+            (range 2 (inc 6)))))
+
+(def wonderland-numbers
+  "Lazy seq of 6 digit wonderland numbers"
+  (filter wonder? (range 100000 999999)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first wonderland-numbers))
