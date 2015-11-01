6a2d9e1a8cd49ebd6698e4ef293817a6c4412adf
devstopfix <james@devstopfix.net>
2015-11-01 09:28:15 +0000

    Try core-logic

--- fox-goose-bag-of-corn/project.clj
+++ fox-goose-bag-of-corn/project.clj
@@ -1,7 +1,8 @@
-(defproject fox-goose-bag-of-corn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject fox-goose-bag-of-corn "0.1.0"
+  :description "Fox, Goose, and Bag of Corn"
+  :url "https://github.com/gigasquid/wonderland-clojure-katas/tree/master/fox-goose-bag-of-corn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.10"]]
   :main fox-goose-bag-of-corn.puzzle)
